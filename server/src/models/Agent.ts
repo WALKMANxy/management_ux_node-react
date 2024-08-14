a7f8dbbb820a3784d83575e752d8eb84e6449d98
WALKMANxy <artorias555@gmail.com>
2024-08-14 17:21:22 +0200

    In the process of creating the webSocket

--- server/src/models/Agent.ts
+++ server/src/models/Agent.ts
@@ -1,22 +1,7 @@
 import { Document, Schema, Types, model } from "mongoose";
 import { IAlert } from "./Alert";
 
-interface IClient extends Document {
-  codice: string;
-  ragioneSociale: string;
-  ragioneSocialeAgg: string;
-  indirizzo: string;
-  capComuneProv: string;
-  telefono: string;
-  email: string;
-  emailPec: string;
-  partitaIva: string;
-  codiceFiscale: string;
-  mp: string;
-  descizioneMetodoPagamento: string;
-  cs: string;
-  ag: string;
-}
+
 
 export interface IAgent extends Document {
   id: string;
@@ -29,22 +14,7 @@ export interface IAgent extends Document {
   AgentPromos: Types.ObjectId[]; // Reference to Promo model
 }
 
-const clientSchema = new Schema<IClient>({
-  codice: { type: String, required: true },
-  ragioneSociale: { type: String, required: true },
-  ragioneSocialeAgg: { type: String, required: false },
-  indirizzo: { type: String, required: true },
-  capComuneProv: { type: String, required: true },
-  telefono: { type: String, required: false },
-  email: { type: String, required: false },
-  emailPec: { type: String, required: false },
-  partitaIva: { type: String, required: true },
-  codiceFiscale: { type: String, required: false },
-  mp: { type: String, required: true },
-  descizioneMetodoPagamento: { type: String, required: true },
-  cs: { type: String, required: true },
-  ag: { type: String, required: true },
-});
+
 
 const agentSchema = new Schema<IAgent>({
   id: { type: String, required: true },
--- server/src/routes/promos.ts
+++ server/src/routes/promos.ts
@@ -1,53 +1,20 @@
-import express, { Request, Response } from "express";
-import { body } from "express-validator";
+import express from "express";
+import { promoValidationRules } from "../constants/validationRules";
+import { PromoController } from "../controllers/promoController";
 import { authenticateUser } from "../middlewares/authentication";
 import {
   checkAgentOrAdminOrClientRole,
   checkAgentOrAdminRole,
 } from "../middlewares/roleChecker";
 import { checkValidation } from "../middlewares/validate";
-import { Promo } from "../models/Promo";
-import { AuthenticatedRequest } from "../models/types";
 
 const router = express.Router();
 
 // Middleware to authenticate and authorize user
 router.use(authenticateUser);
 
-// Validation rules
-const promoValidationRules = [
-  body("clientsId").isArray().withMessage("Clients ID must be an array"),
-  body("type").notEmpty().withMessage("Type is required"),
-  body("name").notEmpty().withMessage("Name is required"),
-  body("discount").notEmpty().withMessage("Discount is required"),
-  body("startDate")
-    .isISO8601()
-    .withMessage("Start date must be a valid ISO 8601 date"),
-  body("endDate")
-    .isISO8601()
-    .withMessage("End date must be a valid ISO 8601 date"),
-  body("promoIssuedBy").notEmpty().withMessage("Promo issued by is required"),
-];
-
 // GET method to retrieve all promos
-router.get(
-  "/",
-  checkAgentOrAdminOrClientRole,
-  async (req: Request, res: Response) => {
-    try {
-      const promos = await Promo.find();
-
-      res.json(promos);
-    } catch (err: unknown) {
-      if (err instanceof Error) {
-        res.status(500).json({ message: err.message });
-      } else {
-        console.error("Unexpected error:", err);
-        res.status(500).json({ message: "Internal Server Error" });
-      }
-    }
-  }
-);
+router.get("/", checkAgentOrAdminOrClientRole, PromoController.getAllPromos);
 
 // POST method to create a new promo
 router.post(
@@ -55,37 +22,7 @@ router.post(
   promoValidationRules,
   checkValidation,
   checkAgentOrAdminRole,
-  async (req: AuthenticatedRequest, res: Response) => {
-    try {
-      const {
-        clientsId,
-        type,
-        name,
-        discount,
-        startDate,
-        endDate,
-        promoIssuedBy,
-      } = req.body;
-      const promo = new Promo({
-        clientsId,
-        type,
-        name,
-        discount,
-        startDate,
-        endDate,
-        promoIssuedBy,
-      });
-      await promo.save();
-      res.status(201).json({ message: "Promo created successfully", promo });
-    } catch (err: unknown) {
-      if (err instanceof Error) {
-        res.status(500).json({ message: err.message });
-      } else {
-        console.error("Unexpected error:", err);
-        res.status(500).json({ message: "Internal Server Error" });
-      }
-    }
-  }
+  PromoController.createPromo
 );
 
 // PUT method to replace an entire promo
@@ -94,25 +31,7 @@ router.put(
   promoValidationRules,
   checkValidation,
   checkAgentOrAdminRole,
-  async (req: AuthenticatedRequest, res: Response) => {
-    try {
-      const promo = await Promo.findByIdAndUpdate(req.params.id, req.body, {
-        new: true,
-        runValidators: true,
-      });
-      if (!promo) {
-        return res.status(404).json({ message: "Promo not found" });
-      }
-      res.status(200).json({ message: "Promo updated successfully", promo });
-    } catch (err: unknown) {
-      if (err instanceof Error) {
-        res.status(500).json({ message: err.message });
-      } else {
-        console.error("Unexpected error:", err);
-        res.status(500).json({ message: "Internal Server Error" });
-      }
-    }
-  }
+  PromoController.replacePromo
 );
 
 // PATCH method to update part of a promo's information
@@ -121,25 +40,7 @@ router.patch(
   promoValidationRules,
   checkValidation,
   checkAgentOrAdminRole,
-  async (req: AuthenticatedRequest, res: Response) => {
-    try {
-      const promo = await Promo.findByIdAndUpdate(req.params.id, req.body, {
-        new: true,
-        runValidators: true,
-      });
-      if (!promo) {
-        return res.status(404).json({ message: "Promo not found" });
-      }
-      res.status(200).json({ message: "Promo updated successfully", promo });
-    } catch (err: unknown) {
-      if (err instanceof Error) {
-        res.status(500).json({ message: err.message });
-      } else {
-        console.error("Unexpected error:", err);
-        res.status(500).json({ message: "Internal Server Error" });
-      }
-    }
-  }
+  PromoController.updatePromo
 );
 
 export default router;
