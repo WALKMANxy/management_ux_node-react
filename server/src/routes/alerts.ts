a50b9333206ddbcf2197a3891efb96f31f08797a
WALKMANxy <artorias555@gmail.com>
2024-08-19 22:29:47 +0200

    Potentially implemented the websocket

--- server/src/routes/alerts.ts
+++ server/src/routes/alerts.ts
@@ -1,10 +1,10 @@
+// routes/alerts.ts
+
 import express from "express";
 import { alertValidationRules } from "../constants/validationRules";
 import {
-  createNewAlert,
-  fetchAlertsByEntity,
-  fetchAllAlerts,
-  updateExistingAlert,
+  AlertController,
+  EmitAlertFunction,
 } from "../controllers/alertController";
 import { authenticateUser } from "../middlewares/authentication";
 import {
@@ -13,46 +13,55 @@ import {
 } from "../middlewares/roleChecker";
 import { checkValidation } from "../middlewares/validate";
 
-const router = express.Router();
+export const setupAlertRoutes = (emitAlert: EmitAlertFunction) => {
+  const router = express.Router();
+  const alertController = new AlertController(emitAlert);
 
-// Middleware to authenticate and authorize user
-router.use(authenticateUser);
+  // Middleware to authenticate and authorize user
+  router.use(authenticateUser);
 
-// GET method to retrieve all alerts
-router.get("/", checkAgentOrAdminOrClientRole, fetchAllAlerts);
+  // GET method to retrieve all alerts
+  router.get(
+    "/",
+    checkAgentOrAdminOrClientRole,
+    alertController.fetchAllAlerts
+  );
 
-// GET method to retrieve alerts by entityRole and entityCode
-router.get(
-  "/:entityRole/:entityCode",
-  checkAgentOrAdminOrClientRole,
-  fetchAlertsByEntity
-);
+  // GET method to retrieve alerts by entityRole and entityCode
+  router.get(
+    "/:entityRole/:entityCode",
+    checkAgentOrAdminOrClientRole,
+    alertController.fetchAlertsByEntity
+  );
 
-// POST method to create a new alert
-router.post(
-  "/",
-  alertValidationRules,
-  checkValidation,
-  checkAgentOrAdminRole,
-  createNewAlert
-);
+  // POST method to create a new alert
+  router.post(
+    "/",
+    alertValidationRules,
+    checkValidation,
+    checkAgentOrAdminRole,
+    alertController.createNewAlert
+  );
 
-// PUT method to replace an entire alert
-router.put(
-  "/:id",
-  alertValidationRules,
-  checkValidation,
-  checkAgentOrAdminRole,
-  updateExistingAlert
-);
+  // PUT method to replace an entire alert
+  router.put(
+    "/:id",
+    alertValidationRules,
+    checkValidation,
+    checkAgentOrAdminRole,
+    alertController.updateExistingAlert
+  );
 
-// PATCH method to update part of an alert's information
-router.patch(
-  "/:id",
-  alertValidationRules,
-  checkValidation,
-  checkAgentOrAdminRole,
-  updateExistingAlert
-);
+  // PATCH method to update part of an alert's information
+  router.patch(
+    "/:id",
+    alertValidationRules,
+    checkValidation,
+    checkAgentOrAdminRole,
+    alertController.updateExistingAlert
+  );
 
-export default router;
+  return router;
+};
+
+export default setupAlertRoutes;
