11d8d23576a0734295db33a257e9fe7323d54a96
WALKMANxy <artorias555@gmail.com>
2024-09-17 02:00:58 +0200

    Hidden https stuff2 2

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,16 +1,16 @@
-import { defineConfig, loadEnv } from 'vite'
+import { defineConfig, /* loadEnv */ } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import { VitePWA } from 'vite-plugin-pwa'
-import fs from 'fs';
-
-const env = loadEnv(
+/* import fs from 'fs';
+ */
+/* const env = loadEnv(
   'all',
   process.cwd()
 );
-
-const devkey = env.VITE_DEV_KEY
+ */
+/* const devkey = env.VITE_DEV_KEY
 const devcrt = env.VITE_DEV_CRT
-
+ */
 
 
 export default defineConfig({
@@ -48,12 +48,12 @@ export default defineConfig({
       }
     })
   ],
-  server: {
+  /* server: {
     https: {
       key: fs.readFileSync(devkey as string),
       cert: fs.readFileSync(devcrt as string),
     },
     host: 'localhost',
     port: 3000,
-  },
+  }, */
 });
