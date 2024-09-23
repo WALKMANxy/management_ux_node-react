fa0d68fc9c311604ec1b5ae6d70b4007aa61ab20
WALKMANxy <artorias555@gmail.com>
2024-09-24 01:59:45 +0200

    We're getting there

--- client/src/hooks/useCalendarWithHolidays.ts
+++ client/src/hooks/useCalendarWithHolidays.ts
@@ -1,21 +1,22 @@
 import { useEffect, useState } from "react";
-import { CalendarEvent, Holiday } from "../models/dataModels";
 import { useGetHolidaysQuery } from "../features/calendar/calendarQuery";
-import { FetchBaseQueryError } from "@reduxjs/toolkit/query";
-import { SerializedError } from "@reduxjs/toolkit";
+import { CalendarEvent, Holiday } from "../models/dataModels";
 
-interface UseCalendarWithHolidaysReturnType {
-    holidayEvents: CalendarEvent[] | undefined;
-    isHolidaysLoading: boolean;
-    holidaysError: FetchBaseQueryError | SerializedError | undefined;
-  }
-  export const useCalendarWithHolidays = (): UseCalendarWithHolidaysReturnType => {
-    const { data: holidays, isLoading: isHolidaysLoading, error: holidaysError } = useGetHolidaysQuery();
-    const [holidayEvents, setHolidayEvents] = useState<CalendarEvent[] | undefined>(undefined);
+export const useCalendarWithHolidays = (currentDate: Date) => {
+  const year = currentDate.getFullYear();
+  const {
+    data: holidays,
+    isLoading: isHolidaysLoading,
+    error: holidaysError,
+  } = useGetHolidaysQuery({ year });
+  const [holidayEvents, setHolidayEvents] = useState<
+    CalendarEvent[] | undefined
+  >(undefined);
 
-    useEffect(() => {
-      if (holidays) {
-        const holidayEventsMapped: CalendarEvent[] = holidays.map((holiday: Holiday) => ({
+  useEffect(() => {
+    if (holidays) {
+      const holidayEventsMapped: CalendarEvent[] = holidays.map(
+        (holiday: Holiday) => ({
           userId: "holiday", // Assign a special ID for holidays
           startDate: new Date(holiday.date),
           endDate: new Date(holiday.date),
@@ -26,15 +27,16 @@ interface UseCalendarWithHolidaysReturnType {
           status: "approved", // Holidays are always approved
           createdAt: new Date(holiday.date),
           updatedAt: new Date(holiday.date),
-        }));
+        })
+      );
 
-        setHolidayEvents(holidayEventsMapped);
-      }
-    }, [holidays]);
+      setHolidayEvents(holidayEventsMapped);
+    }
+  }, [holidays]);
 
-    return {
-      holidayEvents,
-      isHolidaysLoading,
-      holidaysError,
-    };
+  return {
+    holidayEvents,
+    isHolidaysLoading,
+    holidaysError,
   };
+};
