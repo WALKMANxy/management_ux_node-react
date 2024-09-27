285e4906b74902708710eee6ccc256906010bde6
WALKMANxy <artorias555@gmail.com>
2024-09-27 09:59:38 +0200

    Implemented automatic messages, starting to refactor the websocket to use rooms instead of iterating over connected clients

--- client/src/utils/chatUtils.ts
+++ client/src/utils/chatUtils.ts
@@ -1,5 +1,6 @@
 import dayjs from "dayjs";
-import { IChat } from "../models/dataModels";
+import i18next from "i18next";
+import { IChat, IMessage, Promo, Visit } from "../models/dataModels";
 
 // Utility to sanitize the search term
 export const sanitizeSearchTerm = (term: string) =>
@@ -83,3 +84,50 @@ export const canUserChat = (chat: IChat, currentUserId: string): boolean => {
   // In all other cases, allow chatting
   return true;
 };
+
+// Type guard for Promo
+function isPromo(data: Promo | Visit): data is Promo {
+  return (data as Promo).promoType !== undefined;
+}
+
+// Type guard for Visit
+function isVisit(data: Promo | Visit): data is Visit {
+  return (data as Visit).visitReason !== undefined;
+}
+
+export function generateAutomatedMessage(
+  data: Promo | Visit
+): Partial<IMessage> {
+  const timestamp = new Date();
+
+  let content = "";
+  let messageType: IMessage["messageType"];
+
+  // Type guards to check the type of data
+  if (isPromo(data)) {
+    messageType = "promo";
+    content = i18next.t("messages.promoMessage", {
+      promoType: data.promoType,
+      promoName: data.name,
+      promoDiscount: data.discount,
+    });
+  } else if (isVisit(data)) {
+    messageType = "visit";
+    content = i18next.t("messages.visitMessage", {
+      visitType: data.type,
+      visitReason: data.visitReason,
+      visitDate: data.date.toLocaleDateString(),
+    });
+  } else {
+    throw new Error("Either a promo or visit object must be provided.");
+  }
+
+  const message: Partial<IMessage> = {
+    content,
+    timestamp,
+    status: "pending",
+    messageType,
+  };
+
+  return message;
+}
